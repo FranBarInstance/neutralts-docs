c9705f3e9fc856e7513ed6711005cb65afd1003c
FranBarInstance <52417652+FranBarInstance@users.noreply.github.com>
2025-09-15 11:41:41 +0200

    fix example

--- examples/rust/helloworld/src/main.rs
+++ examples/rust/helloworld/src/main.rs
@@ -1,5 +1,5 @@
 /**
- * Neutral TS Hello World package example
+ * Neutral TS Hello World Rust example
  * https://github.com/FranBarInstance/neutralts-docs/
  */
 
@@ -14,18 +14,23 @@ fn main() {
         }
     });
 
-
-    // Determine the template path and read content
-    let template_path = "template.ntpl";
+    // Determine the template full path
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    let template_path = format!("{}/template.ntpl", manifest_dir);
 
     // Create an instance of Template
-    let mut template = Template::from_file_value(template_path, schema).unwrap();
+    let mut template = Template::from_file_value(&template_path, schema).unwrap();
 
     // Render the template
     let contents = template.render();
 
+    // Get the status code, text and parameter
+    let status_code: &str = template.get_status_code();
+    let status_text: &str = template.get_status_text();
+    let status_param: &str = template.get_status_param();
+
     // Print the rendered content, in other cases contents will be sent to output according to framework.
     println!("{}", contents);
-    println!("Status code: {} {}", template.get_status_code(), template.get_status_text());
+    println!("Status: {} {} {}", status_code, status_text, status_param);
     println!("Render time: {:?}", template.get_time_duration());
 }
